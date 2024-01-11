f6c42b2fff331e86b47fab9611be72c1e55231ee
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-01-11 15:41:06 +0300

    1

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+		<a href=''>
+			<img src='' alt='' />
+		</a>
+	)
+}
+
+export default Logo;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from '../../components/logo/Logo';
+import { Menu } from '../../components/menu/Menu';
+
+// Логотип и верхняя меню
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo/>
+      <Menu/>
+    </StyledHeader>
+  )
+}
+
+const StyledHeader = styled.header`
+  background-color: #87ca87
+`
+
+
